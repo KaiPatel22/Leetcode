1dfaf9db86ea251e5c3ce8d1a1068b663a28ae53
KaiPatel22 <kaipatel424@gmail.com>
2024-11-15 11:06:00 +0000

    first commit

---
+++ 217ContainsDuplicates.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> newList = new HashSet<Integer>();
+        for (int num : nums){
+            if (newList.contains(num)){
+                return true;
+            }
+            newList.add(num);
+        }
+        return false;
+    }
+}
