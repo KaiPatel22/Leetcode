5f9d16d8ed560a9afd758ff5a9b8115ed7edb13a
KaiPatel22 <kaipatel424@gmail.com>
2024-11-19 11:10:46 +0000

    My solution for leetcode problem 3110 - Score of a String

---
+++ 3110ScoreOfAString.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int score = 0;
+        for (int i = 0; i < s.length() - 1; i++){
+            char currentChar = s.charAt(i);
+            char nextChar = s.charAt(i+1);
+            
+            score += Math.abs((int)currentChar - (int)nextChar);
+        }
+        return score;
+    }
+}
